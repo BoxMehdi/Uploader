021ffafd84a9476297848457affb3f317c2ef831
BoxMehdi <m.sml@gmx.at>
2025-07-12 11:43:40 +0200

    database.py

--- database.py
+++ database.py
@@ -1,32 +1,33 @@
-import pymongo
-from config import MONGO_URI, MONGO_DB
+from pymongo import MongoClient
+import os
 
 class MongoDBClient:
     def __init__(self):
-        self.client = pymongo.MongoClient(MONGO_URI)
-        self.db = self.client[MONGO_DB]
-        self.files = self.db["files"]
-        self.users = self.db["users"]
+        self.client = MongoClient(os.getenv("MONGO_URI"))
+        self.db = self.client[os.getenv("MONGO_DB")]
+        self.files_col = self.db.files
+        self.users_col = self.db.users
 
-    # بقیه توابع بدون تغیی
-
-    def save_file(self, film_id, file_id, caption, channel):
-        self.files.insert_one({
+    def save_file(self, film_id, file_id, caption, quality, channel):
+        # یک سند جدید اضافه کن برای هر فایل با کیفیتش
+        self.files_col.insert_one({
             "film_id": film_id,
             "file_id": file_id,
             "caption": caption,
+            "quality": quality,
             "channel": channel,
             "views": 0
         })
 
     def get_files(self, film_id):
-        return list(self.files.find({"film_id": film_id}))
+        return list(self.files_col.find({"film_id": film_id}))
 
     def increment_views(self, film_id):
-        self.files.update_many({"film_id": film_id}, {"$inc": {"views": 1}})
+        self.files_col.update_many({"film_id": film_id}, {"$inc": {"views": 1}})
 
     def has_seen_welcome(self, user_id):
-        return self.users.find_one({"_id": user_id}) is not None
+        user = self.users_col.find_one({"user_id": user_id})
+        return user and user.get("seen_welcome", False)
 
     def mark_seen(self, user_id):
-        self.users.insert_one({"_id": user_id})
+        self.users_col.update_one({"user_id": user_id}, {"$set": {"seen_welcome": True}}, upsert=True)
