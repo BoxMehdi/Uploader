544b793038e2af5e46c76993f5d7d836b3e41f47
BoxMehdi <m.sml@gmx.at>
2025-07-12 12:26:33 +0200

    scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,8 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import datetime
-import asyncio
 
 scheduler = AsyncIOScheduler()
-scheduler.start()
 
-def schedule_post(client, film_id, file_id, caption, schedule_time_str, channel_username):
-    dt = datetime.strptime(schedule_time_str, "%Y-%m-%d %H:%M")
-    
-    async def job():
-        msg = await client.send_document(channel_username, file_id, caption=caption)
-        await asyncio.sleep(30)
-        await msg.delete()
-
-    scheduler.add_job(lambda: asyncio.create_task(job()), 'date', run_date=dt)
+def schedule_post(app, film_id, file_id, caption, post_time, channel_username):
+    async def post_file():
+        await app.send_document(chat_id=channel_username, document=file_id, caption=caption)
+    scheduler.add_job(post_file, "date", run_date=post_time)
