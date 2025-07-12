0af84752964c1036354b1c18409c0e1d6d7c7359
BoxMehdi <m.sml@gmx.at>
2025-07-12 12:25:34 +0200

    config.py

--- config.py
+++ config.py
@@ -6,7 +6,10 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS").split(",")))
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB = os.getenv("MONGO_DB")
-ADMINS = list(map(int, os.getenv("ADMIN_IDS").split(",")))
-BOT_USERNAME = os.getenv("BOT_USERNAME")
+
+REQUIRED_CHANNELS = list(map(int, os.getenv("REQUIRED_CHANNELS").split(",")))
