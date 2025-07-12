db0b2e27d8a5390d4dc976b68266b791fe843a05
BoxMehdi <m.sml@gmx.at>
2025-07-12 12:27:01 +0200

    check_subscription.py

--- check_subscription.py
+++ check_subscription.py
@@ -1,14 +1,14 @@
-import os
-from pyrogram import Client
+from config import REQUIRED_CHANNELS
+from pyrogram.errors import UserNotParticipant
 
-CHANNEL_IDS = list(map(int, os.getenv("REQUIRED_CHANNELS").split(",")))
-
-async def check_user_subscribed(client: Client, user_id: int) -> bool:
-    for ch_id in CHANNEL_IDS:
+async def check_user_subscribed(client, user_id):
+    for channel_id in REQUIRED_CHANNELS:
         try:
-            member = await client.get_chat_member(ch_id, user_id)
-            if member.status not in ["member", "administrator", "creator"]:
+            member = await client.get_chat_member(channel_id, user_id)
+            if member.status in ("left", "kicked"):
                 return False
+        except UserNotParticipant:
+            return False
         except:
             return False
     return True
