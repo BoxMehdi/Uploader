cfb078a995a771fae836b0519d7f257501015be2
BoxMehdi <m.sml@gmx.at>
2025-07-12 16:36:52 +0200

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import logging
-from pyrogram import Client
+from pyrogram import Client, idle
 from config import API_ID, API_HASH, BOT_TOKEN
-import bot, callbacks  # imports handlers
+from keep_alive import keep_alive
+from bot import register_handlers
+from callbacks import register_callbacks
+
+app = Client("BoxOfficeUploaderBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+register_handlers(app)
+register_callbacks(app)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    app = Client("BoxOfficeBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-    app.include(bot.app)
-    app.include(callbacks.app)
-    app.run()
+    keep_alive()
+    app.start()
+    print("✅ Bot is running!")
+    idle()
+    app.stop()
